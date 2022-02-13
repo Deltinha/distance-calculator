a20640c348c43282f4da0b5f3908a594a6b47627
Moisés Brandão <moisesbrandao1998@gmail.com>
2022-02-13 01:14:29 -0300

    Get distances endpoint now returns the farest and shortest distances

--- src/services/distanceService.ts
+++ src/services/distanceService.ts
@@ -1,5 +1,10 @@
 import { Location } from '../interfaces/location';
 
+interface Distance {
+  addresses: string[];
+  distance: string;
+}
+
 function getMinAngle(A: number, B: number) {
   function getCongruentAngle(angle: number) {
     return angle - Math.floor(angle / 360) * 360;
@@ -14,15 +19,24 @@ function calculateDistance(A: number, B: number) {
 
   const degreeDistance = Math.sqrt(A ** 2 + B ** 2);
 
-  return ((2 * Math.PI * EARTH_RADIUS * degreeDistance) / 360).toFixed(2);
+  return (2 * Math.PI * EARTH_RADIUS * degreeDistance) / 360;
 }
 
-export function getDistances(locations: Location[]) {
-  interface Distance {
-    addresses: string[];
-    distance: string;
-  }
-  const distances: Distance[] = [];
+export function getDistances(locations: Location[]): {
+  farestDistance: Distance;
+  shortestDistance: Distance;
+  allDistances: Distance[];
+} {
+  const allDistances: Distance[] = [];
+
+  let shortestDist = Infinity;
+  let farestDist = -Infinity;
+
+  const distances = {
+    farestDistance: { addresses: [''], distance: '' },
+    shortestDistance: { addresses: [''], distance: '' },
+    allDistances,
+  };
 
   for (let firstIndex = 0; firstIndex < locations.length; firstIndex += 1) {
     for (
@@ -39,12 +53,36 @@ export function getDistances(locations: Location[]) {
         locations[secondIndex].location.lng
       );
 
-      distances.push({
+      const distance = calculateDistance(latitudeDelta, longitudeDelta);
+
+      if (distance >= farestDist) {
+        farestDist = distance;
+        distances.farestDistance = {
+          addresses: [
+            locations[firstIndex].address,
+            locations[secondIndex].address,
+          ],
+          distance: `${distance.toFixed(2)} km`,
+        };
+      }
+
+      if (distance <= shortestDist) {
+        shortestDist = distance;
+        distances.shortestDistance = {
+          addresses: [
+            locations[firstIndex].address,
+            locations[secondIndex].address,
+          ],
+          distance: `${distance.toFixed(2)} km`,
+        };
+      }
+
+      allDistances.push({
         addresses: [
           locations[firstIndex].address,
           locations[secondIndex].address,
         ],
-        distance: `${calculateDistance(latitudeDelta, longitudeDelta)} km`,
+        distance: `${distance.toFixed(2)} km`,
       });
     }
   }
